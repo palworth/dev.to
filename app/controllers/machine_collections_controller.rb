b34607e825744b33be05af22b5b88708d236a0bc
Maria Ronauli <mronauli@gmail.com>
2020-05-13 14:30:03 -0600

    add show action to machine collections controller

--- app/controllers/machine_collections_controller.rb
+++ app/controllers/machine_collections_controller.rb
@@ -3,12 +3,18 @@ class MachineCollectionsController < ApplicationController
   before_action :authenticate_user!, except: %i[feed new]
 
   def index
-    @machine_collections = current_user.machine_collections
+    machine_collections = current_user.machine_collections
+    render json: MachineCollectionSerializer.new(machine_collections)
+  end
+
+  def show
+    machine_collection = current_user.machine_collections.find(params[:id])
+    render json: MachineCollectionSerializer.new(machine_collection)
   end
 
 private
   def machine_collection_params
-   params.require(:user).permit(:name, :tag_list, :user_id)
+    allowed_params = %i[title user_id tag_list]
+    params.require(:machine_collections).permit(allowed_params)
   end
-
 end
