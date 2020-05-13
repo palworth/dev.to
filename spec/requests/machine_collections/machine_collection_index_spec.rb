bdde9936f9f723bb9b071ad2b0d2e30992f4a09e
Maria Ronauli <mronauli@gmail.com>
2020-05-13 14:23:16 -0600

    add to machine collections index spec

--- spec/requests/machine_collections/machine_collection_index_spec.rb
+++ spec/requests/machine_collections/machine_collection_index_spec.rb
@@ -4,17 +4,36 @@ RSpec.describe "Machine Collections", type: :request do
   let(:user) { create(:user) }
   let(:tag1)  { create(:tag) }
   let(:tag2)  { create(:tag) }
-
+  let(:tag3)  { create(:tag) }
 
   describe "get index page machine_collections/index" do
     it "User can visit thier machine colleciton index" do
       sign_in user
-      user.machine_collections.create!(title: "Ruby collection")
+      ruby_col = user.machine_collections.create(title: "Ruby collection")
+      ruby_col.tag_list.add(tag1, tag2)
+      ruby_col.save
       create(:article, tags: %w[ruby java rails])
       create(:article, tags: %w[ruby javascript rails])
       create(:article, tags: %w[rails preact])
-      user.machine_collections.first.articles << Article.all
-      get "/machinecollections"
+      ruby_col.articles << Article.all
+      testing = user.machine_collections.create(title: "Testing")
+      testing.tag_list.add(tag3)
+      testing.save
+      get machinecollections_path
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq("application/json")
+    end
+
+    it "can get one of its collections" do
+      sign_in user
+      ruby_col = user.machine_collections.create!(title: "Ruby collection")
+      ruby_col.tag_list.add(tag1)
+      ruby_col.save
+      create(:article, tags: %w[ruby java rails])
+      create(:article, tags: %w[ruby javascript rails])
+      create(:article, tags: %w[rails preact])
+      ruby_col.articles << Article.all
+      get "/machinecollections/#{ruby_col.id}"
       expect(response.status).to eq(200)
       expect(response.content_type).to eq("application/json")
     end
